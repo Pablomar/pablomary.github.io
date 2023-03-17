e1f468d6b2a324981bf96be77bc9a799491376df
Pablomar <123875962+Pablomar@users.noreply.github.com>
2023-03-17 12:17:03 +0100

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,6 @@
+
+<>
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Email</button>
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Telefono</button>
+</>
+
